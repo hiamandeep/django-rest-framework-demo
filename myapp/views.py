d131a2131d3a4700017911bfd18f4ac1a4a00b26
Aman Deep <amandeep1024@gmail.com>
2018-10-07 15:23:39 +0530

    add session authentication

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from django.contrib.auth import login, logout, authenticate
+
 
 
 # Create your views here.
@@ -20,3 +22,6 @@ class ProductView(viewsets.ModelViewSet):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer
 
+def logoutView(request):
+    logout(request)
+    return redirect('login')
